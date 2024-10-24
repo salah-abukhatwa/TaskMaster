12652dab9bf061de3577c99f42dbdaae2e31ee26
salah-abukhatwa <roben.gn2@gmail.com>
2024-10-24 14:04:53 +0300

    creat user component

---
+++ src/app/user/user.component.ts
@@ -0,0 +1,22 @@
+import { Component, Input, input } from '@angular/core';
+import { DUMMY_USERS } from '../dummy-users';
+
+const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+
+@Component({
+  selector: 'app-user',
+  standalone: true,
+  imports: [],
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.css',
+})
+export class UserComponent {
+  @Input() avatar!: string;
+  @Input() name!: string;
+
+  get imagePath() {
+    return 'assets/users/' + this.avatar;
+  }
+
+  onSelectUser() {}
+}
