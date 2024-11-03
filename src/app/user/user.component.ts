fa8abea5d5b3017b4b7b7a76f0ee1f3eaf81f714
salah-abukhatwa <roben.gn2@gmail.com>
2024-11-03 21:08:37 +0300

    create tasks component

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, Input, input, Output } from '@angular/core';
 import { DUMMY_USERS } from '../dummy-users';
+import { User } from './user.model';
 
 const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 
@@ -11,15 +12,15 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.component.css',
 })
 export class UserComponent {
-  @Input({ required: true }) id!: string;
-  @Input({ required: true }) avatar!: string;
-  @Input({ required: true }) name!: string;
+  @Input({ required: true }) user!: User;
+  @Input({ required: true }) selected!: boolean;
+
   @Output() select = new EventEmitter();
   get imagePath() {
-    return 'assets/users/' + this.avatar;
+    return 'assets/users/' + this.user.avatar;
   }
 
   onSelectUser() {
-    this.select.emit(this.id);
+    this.select.emit(this.user.id);
   }
 }
